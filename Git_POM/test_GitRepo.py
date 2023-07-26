c43ff786d355a54d29c0d9aa7ad91dfee6c4b96f
Ali Rana <alirana78662@gmail.com>
2023-07-27 01:03:23 +0500

    updated1.2

--- Git_POM/test_GitRepo.py
+++ Git_POM/test_GitRepo.py
@@ -3,8 +3,8 @@ from Git_UI_TestCases.test_git_repo import GitRepo
 
 
 def test_git_repo_ui(page: Playwright) -> None:
-    browser = page.chromium.launch()
-    page = browser.new_page()
+    # browser = page.chromium.launch()
+    # page = browser.new_page()
     git = GitRepo(page)
     git.navigate()
     git.login()
