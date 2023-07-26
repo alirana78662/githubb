5f2a5b47da25eccc72ac77a8b969fe8825a4be9c
Ali Rana <alirana78662@gmail.com>
2023-07-26 23:19:06 +0500

    updated1.0

--- reuse_auth/test_login.py
+++ reuse_auth/test_login.py
@@ -1,21 +1,21 @@
-import time
-
-from playwright.sync_api import Playwright, sync_playwright
-
-
-def test_login(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
-    page.goto("https://github.com/")
-    page.locator("//a[contains(text(), 'Sign in')]").click()
-    page.locator("//input[@id = 'login_field']").fill('alirana78662@gmail.com')
-    page.locator("//input[@id = 'password']").click()
-    page.locator("//input[@id = 'password']").fill('Mcc02151001@')
-    page.locator("//input[@name = 'commit']").click()
-    time.sleep(4)
-
-    # Save storage state into the file.
-    context.storage_state(path="state.json")
-
-
+# import time
+#
+# from playwright.sync_api import Playwright, sync_playwright
+#
+#
+# def test_login(playwright: Playwright) -> None:
+#     browser = playwright.chromium.launch(headless=False)
+#     context = browser.new_context()
+#     page = context.new_page()
+#     page.goto("https://github.com/")
+#     page.locator("//a[contains(text(), 'Sign in')]").click()
+#     page.locator("//input[@id = 'login_field']").fill('alirana78662@gmail.com')
+#     page.locator("//input[@id = 'password']").click()
+#     page.locator("//input[@id = 'password']").fill('Mcc02151001@')
+#     page.locator("//input[@name = 'commit']").click()
+#     time.sleep(4)
+#
+#     # Save storage state into the file.
+#     context.storage_state(path="state.json")
+#
+#
