0425b5ebd9c6699517e4c38518e5fb7bf842b72a
Ali Rana <alirana78662@gmail.com>
2023-07-26 23:30:49 +0500

    updated1.1

--- Git_POM/test_GitRepo.py
+++ Git_POM/test_GitRepo.py
@@ -8,5 +8,5 @@ def test_git_repo_ui(page: Playwright) -> None:
     git = GitRepo(page)
     git.navigate()
     git.login()
-    git.Add_Git_Repo_Ui()
-    git.Del_Git_Repo_Ui()
+    # git.Add_Git_Repo_Ui()
+    # git.Del_Git_Repo_Ui()
