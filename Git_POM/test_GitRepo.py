287e68f3aa14c4bb116e2cb746877257d1ced82d
Ali Rana <alirana78662@gmail.com>
2023-07-26 23:59:20 +0500

    updated1.2

--- Git_POM/test_GitRepo.py
+++ Git_POM/test_GitRepo.py
@@ -1,10 +1,14 @@
+import os
+
 from playwright.sync_api import Playwright
 from Git_UI_TestCases.test_git_repo import GitRepo
 
 
 def test_git_repo_ui(playwright: Playwright) -> None:
     browser = playwright.chromium.launch()
-    context = browser.new_context(storage_state="state.json")
+    file_name = "state.json"
+    file_path = os.path.abspath(file_name)
+    context = browser.new_context(storage_state=file_path)
     page = context.new_page()
     git = GitRepo(page)
     git.navigate()
